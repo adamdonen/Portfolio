a16f60811c1aefbf845e513a169fb2486c82af4a
Adam Donen <donena@myumanitoba.ca>
2023-04-29 13:19:43 -0500

    added javascript animation and adjusted site layout

--- script.js
+++ script.js
@@ -1,48 +1,39 @@
-// const projectList = document.getElementById("aboutlink");
-//
-// let scrollPos = 0;
-// let ticking = false;
-//
-// function doSomething() {
-//     // do something with scroll position
-//     console.log("Hello")
-// }
-//
-// projectList.addEventListener("click", function (){
-//    doSomething()
-// });
-//
-// function resizeText(multiplier) {
-//     if (document.body.style.fontSize == "") {
-//         document.body.style.fontSize = "1.0em";
-//     }
-//     document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 0.2) + "em";
-// }
-//
+// Initialising the canvas
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
 
-function finalGrade(a,b,c){
-    let avg = Math.floor((a+b+c)/3.0);
-    if ((a < 0 || a > 100) || (b < 0 || b > 100) || (c < 0 || c >100)){
-        return "You have entered an invalid grade";
-    }else{
-        switch(true){
-            case avg < 60:
-                return "F";
-                break;
-            case avg >=60 && avg < 70:
-                return "D";
-                break;
-            case avg >=70 && avg < 80:
-                return 'C';
-                break;
-            case avg >=80 && avg < 90:
-                return 'B';
-                break;
-            case avg >= 90 && avg <= 100:
-                return 'A';
-                break;
+// Setting the width and height of the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Setting up the letters
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+letters = letters.split('');
+
+// Setting up the columns
+var fontSize = 10,
+    columns = canvas.width / fontSize;
+
+// Setting up the drops
+var drops = [];
+for (var i = 0; i < columns; i++) {
+    drops[i] = 1;
+}
+
+// Setting up the draw function
+function draw() {
+    ctx.fillStyle = 'rgba(45,46,45, .25)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < drops.length; i++) {
+        var text = letters[Math.floor(Math.random() * letters.length)];
+        ctx.fillStyle = '#0f0';
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+        drops[i]++;
+        if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+            drops[i] = 0;
         }
     }
 }
-// Uncomment the line below when you're ready to try out your function
-console.log(finalGrade(-43, 92, 95)) // Should print 'A'
+
+// Loop the animation
+setInterval(draw, 33);
